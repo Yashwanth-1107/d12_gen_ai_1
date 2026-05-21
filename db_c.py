e7fd0bf4e81d8b6928a7c081e9156ade2b733da3
Ambati Yashwanth <yashwanth.ambati321@gmail.com>
2026-05-21 10:18:23 +0530

    updatedd code

--- db_c.py
+++ db_c.py
@@ -13,8 +13,8 @@ cursor = conn.cursor()
 
 # USERS TABLE
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users3(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users(
+    id int PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE,
     password VARCHAR(100)
@@ -24,13 +24,13 @@ CREATE TABLE IF NOT EXISTS users3(
 # FILES TABLE
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS files(
-    id SERIAL PRIMARY KEY,
+    id int PRIMARY KEY,
     user_id INT,
     file_name VARCHAR(255),
     file_type VARCHAR(100),
     file_url TEXT,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY(user_id) REFERENCES users3(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 )
 """)
 
