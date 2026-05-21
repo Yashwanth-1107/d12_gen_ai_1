fd367419550dfcd872deff40793fd8bd3c4cb642
Ambati Yashwanth <yashwanth.ambati321@gmail.com>
2026-05-21 10:24:10 +0530

    updated code

--- db_c.py
+++ db_c.py
@@ -14,7 +14,7 @@ cursor = conn.cursor()
 # USERS TABLE
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
-    id int PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE,
     password VARCHAR(100)
@@ -24,19 +24,16 @@ CREATE TABLE IF NOT EXISTS users(
 # FILES TABLE
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS files(
-    id int PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     file_name VARCHAR(255),
     file_type VARCHAR(100),
     file_url TEXT,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 )
 """)
 
 conn.commit()
 
 print("Tables Created Successfully")
-
-#pip install streamlit mysql-connector-python
-
